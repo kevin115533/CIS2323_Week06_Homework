62cd3ab474941f65317adef3ae61cac4bb4ff5fd
kevin115533 <kevin115533@yahoo.com>
2018-10-01 23:21:05 -0500

    added some things and it compiles

--- CheckersPiece/CheckersPiece.java
+++ CheckersPiece/CheckersPiece.java
@@ -1,6 +1,33 @@
 package CheckersPiece;
 
 public class CheckersPiece{
+	private char color;
+	private int xPos,yPos;
+	
+	public void makePiece(char x, int y, int z){
+		setColor(x);
+		setPos(y,z);
+	}
 
+	public void setColor(char x){
+		color = x;	
+	}
 
+	public char getColor(){
+		return color;
+	}
+	
+	public void setPos(int x, int y){
+		xPos = x;
+		yPos = y;	
+	}
+	
+	public String getPos(){
+		return xPos + yPos;	
+	}
+	
+	public String printPiece(){
+		return color + xPos + yPos;	
+	}
+	
 }
--- SetupCheckerBoard.java
+++ SetupCheckerBoard.java
@@ -1,10 +1,25 @@
 import CheckersPiece.*;
 import Board.*;
+import java.util.Scanner;
 
 public class SetupCheckerBoard{
 	public static void main (String[] args){
+		char userOpt;
+		final int count = 12;
+		Scanner input1 = new Scanner(System.in);
+		
+		System.out.println("Play a game of checkers? y/n");
+		userOpt = input1.next().charAt(0);
+		
+		
+		if(userOpt == 'y'){
 		Board newGame = new Board();
+		CheckersPiece[] red = new CheckersPiece[count];
 		
 		newGame.printBoard();
+			
+			
+			
+		}
 	}
 }
