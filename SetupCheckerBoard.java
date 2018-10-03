7171e5d02abc3513078028fe0643c215beb68495
kevin115533 <kevin115533@yahoo.com>
2018-10-02 23:52:37 -0500

    still working on the X-coordinate for pieces

--- SetupCheckerBoard.java
+++ SetupCheckerBoard.java
@@ -21,13 +21,13 @@ public class SetupCheckerBoard{
 			for(int i = 0;i<count;i++){
 				red[i] = new CheckersPiece();
 				if(i<4){
-					red[i].makePiece("RED",1,0);	
+					red[i].makePiece("R",1,0);	
 				}
 				if(i>=4 && i<8){
-					red[i].makePiece("RED",1,1);
+					red[i].makePiece("R",1,1);
 				}
 				if(i>=8){
-					red[i].makePiece("RED",1,2);
+					red[i].makePiece("R",1,2);
 				}
 			}
 			
@@ -36,13 +36,13 @@ public class SetupCheckerBoard{
 			for(int i = 0;i<count;i++){
 				blk[i] = new CheckersPiece();
 				if(i<4){
-					blk[i].makePiece("BLK",1,0);	
+					blk[i].makePiece("B",1,5);	
 				}
 				if(i>=4 && i<8){
-					blk[i].makePiece("BLK",1,1);
+					blk[i].makePiece("B",1,6);
 				}
 				if(i>=8){
-					blk[i].makePiece("BLK",1,2);
+					blk[i].makePiece("B",1,7);
 				}
 			}
 			
